5b4203f534dfc4a4e92c73691ff937c15dea8115
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-11 23:54:49 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -129,18 +129,19 @@ class ID_control:
             return
 
         passed_time = rospy.get_time() - self.start_time
-        if passed_time > 3.7:
+        if passed_time > 4.7:
             self.flag = None
             self.override_twist = False
             self.park = False
             # rospy.loginfo("PARK Marker End")
-        elif passed_time > 2.7:
+        elif passed_time > 3.7:
+            rospy.sleep(1.)
             self.drive_data.linear.x = -0.3
             self.drive_data.angular.z = 0.0
         elif passed_time > 1.7:
             self.drive_data.linear.x = 0.3
             self.drive_data.angular.z = 0.0
-        elif passed_time > 0.2:
+        elif passed_time > 0.3:
             self.override_twist = True
             self.drive_data.linear.x = 0.0
             self.drive_data.angular.z = -1.0 # 주차는 값 조정 필요
