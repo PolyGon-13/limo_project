d0fc09650b26283a40d5400c89eb37fcf3fb6284
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-12 21:30:31 +0900

    파이썬의 모든 코드는 순서에 따른 우선순위가 있다!
    by. 박현우, 박하윤 선배님

--- src/limo_legend/scripts/control.py
+++ src/limo_legend/scripts/control.py
@@ -130,6 +130,10 @@ class LimoController:
         drive_data.angular.z = (self.distance_left + self.distance_right) * self.LATERAL_GAIN
 
         try:
+            # 라인 겹침 처리
+            if self.lane_connected == True: # 왼쪽 차선이 2번째 카메라(오른쪽 차선)에 침범한 경우
+                drive_data.angular.z = self.distance_left * self.LATERAL_GAIN # 오른쪽 카메라의 계산값은 무시하고 왼쪽 차선 정보를 이용
+
             # 마커 감지 유무에 따른 마커 동작
             if self.override_twist == True: # 마커를 인식한 경우
                 # if self.e_stop != "Warning": # 라이다가 장애물을 감지하지 않았을 경우
@@ -139,10 +143,6 @@ class LimoController:
                     # 계산된 각속도가 0.3보다 작거나(가속을 하면 안되는 구간에서도 두 차선을 인식하는 경우가 발생하기 때문에 사용)
                     # 또는 주차 마커를 인식한 경우(교차로 구간에서 가속을 하는 구간이 발생하는데 주차 모션 제어가 방해가 됨)
                     drive_data.linear.x *= 1.3 # 기존 속도의 1.3배로 달림
-
-            # 라인 겹침 처리
-            if self.lane_connected == True: # 왼쪽 차선이 2번째 카메라(오른쪽 차선)에 침범한 경우
-                drive_data.angular.z = self.distance_left * self.LATERAL_GAIN # 오른쪽 카메라의 계산값은 무시하고 왼쪽 차선 정보를 이용
 
             # IMU 센서 동작
             if abs(self.angular_y) > 0.05:
