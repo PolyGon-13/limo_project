111ccf5073399c2c2edf936f06528af05d9def7d
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-16 23:07:59 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -187,7 +187,10 @@ class ID_control:
             self.drive_data.linear.x = 0.0
             self.drive_data.angular.z = -1.0
         elif passed_time > 2.5: # gtan 값을 이용해 주차공간 내에서 정렬
-            while abs(self.gtan) > 0.05:
+            if abs(self.gtan) < 0.0005:
+                self.drive_data.linear.x = 0.0
+                self.drive_data.angular.z = 0.0
+            else:
                 self.drive_data.linear.x = 0.0
                 self.drive_data.angular.z = 0.5
         elif passed_time > 1.5: # 조금 직진하여 주차공간에 완벽히 진입
