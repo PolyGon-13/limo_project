d57268ef689c9780db01e52bf72c3d4341733a50
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-16 22:57:23 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -178,12 +178,12 @@ class ID_control:
             return
 
         passed_time = rospy.get_time() - self.start_time
-        if passed_time > 8:
+        if passed_time > 7:
             self.flag = None # 다음 마커 동작 수행을 위해 self.flag 초기화
             self.override_twist = False # control.py에 마커 동작 수행이 끝났음을 알려줄 변수를 False로 전환
             self.park = False # 주차 마커 인식 여부를 False로 전환 (다시 가속 가능)
             # rospy.loginfo("PARK Marker End")
-        elif passed_time > 5: # 오른쪽으로 180도 제자리 회전
+        elif passed_time > 4: # 오른쪽으로 180도 제자리 회전
             self.drive_data.linear.x = 0.0
             self.drive_data.angular.z = -1.0
         elif passed_time > 2.5: # gtan 값을 이용해 주차공간 내에서 정렬
