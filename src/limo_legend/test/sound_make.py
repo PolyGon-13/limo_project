e8523cdef008d6ac8aa2d4ad3bcb77938a359216
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-17 15:50:27 +0900

    Update sound_make.py

--- src/limo_legend/test/sound_make.py
+++ src/limo_legend/test/sound_make.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import os
 
-text = "right"
-tts = gTTS(text=text, lang='en')
-tts.save("right.mp3")
+text = "공습경보"
+tts = gTTS(text=text, lang='ko')
+tts.save("fff.mp3")
 print("end")
