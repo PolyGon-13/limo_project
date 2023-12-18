7406096b881f7bfd6ce8293d7224be13a5ff2058
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-19 00:19:09 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -122,7 +122,7 @@ class ID_control:
             return
 
         passed_time = rospy.get_time() - self.start_time
-        if passed_time > 2:
+        if passed_time > 3:
             #self.flag = None # 다음 마커 동작 수행을 위해 self.flag 초기화
             #self.override_twist = False # control.py에 마커 동작 수행이 끝났음을 알려줄 변수를 False로 전환
             self.park = False # 주차 마커 인식 여부를 False로 전환 (다시 가속 가능)
@@ -136,7 +136,7 @@ class ID_control:
             self.override_twist = True
             self.park = True # 주차 마커를 인식했음을 알림 (가속 차단 용도)
             self.drive_data.linear.x = 0.2
-            self.drive_data.angular.z = -1.2
+            self.drive_data.angular.z = -1.15
 
     
     # 마커들의 동작을 우선순위를 두어 함수 실행 & 주행 데이터와 마커 인식 유무 데이터 퍼블리시
