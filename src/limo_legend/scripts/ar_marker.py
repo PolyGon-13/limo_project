b3ad4a9831be7afb327efa5d12f554b668a0c643
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-18 22:45:01 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -17,6 +17,7 @@ class ID_control:
         self.park = False # 주차 마커를 인식했는지 여부를 담는 변수
         self.right_good = False
         self.stop = False
+        self.collect = None
         self.start_time = rospy.get_time() # 마커 동작을 수행할 때 딜레이를 주기 위해 마커를 인식한 시점에서의 시간을 저장
         self.rate = rospy.Rate(5) # 1초에 5번 loop를 반복할 수 있도록 rate라는 객체를 생성
         self.gtan = 0 # 두 차선의 기울기를 이용해 차선이 어느 한 쪽으로 치우친 정도를 저장
@@ -53,12 +54,13 @@ class ID_control:
     
     # 전달받은 문자열을 저장하고 특정 조건을 만족하면 동작 수행
     def found_sign(self, _data):
+        self.collect = _data
         if self.flag == None: # 전달받은 마커 데이터가 없거나, 마커 동작 수행을 끝마쳐 self.flag에 아무 데이터가 없는 경우
             if self.kim_distance > 0.8 and _data == "park": # 마커와의 거리가 0.8보다 큰데 park 신호가 왔을 경우
                 return
             else:
                 self.start_time = rospy.get_time()
-                self.flag = _data # 저장해둔 문자열을 self.flag로 전달
+                self.flag = self.collect # 저장해둔 문자열을 self.flag로 전달
 
     # 0번 마커(정지 신호)를 인식하였다면 아래의 동작 수행
     def stop_sign(self):
