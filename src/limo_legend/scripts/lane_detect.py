5be3645e2024fe957cbbbeda23449d091413f0e9
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-18 22:38:44 +0900

    Update lane_detect.py

--- src/limo_legend/scripts/lane_detect.py
+++ src/limo_legend/scripts/lane_detect.py
@@ -1,193 +1,112 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import rospy
-from ar_track_alvar_msgs.msg import AlvarMarkers
-from geometry_msgs.msg import Twist
-from std_msgs.msg import Bool, Int32, Float64
-import time
-#import pygame
+from cv_bridge import CvBridge
+from sensor_msgs.msg import CompressedImage, Image
+from std_msgs.msg import Int32, Bool, Float64
+import cv2
+import numpy as np
 
-class ID_control:
+class LaneDetection:
     def __init__(self):
-        rospy.init_node("aruco")
-        self.drive_data = Twist() # 주행 데이터를 담을 drive_data를 Twist 메시지로 선언
-        self.override_twist = False # aruco marker를 인식했는지 여부를 저장
-        self.kim_distance=0  # 수학적으로 계산 마커와의 거리를 저장
-        self.flag = None # id값에 해당하는 문자열을 저장
-        self.park = False # 주차 마커를 인식했는지 여부를 담는 변수
-        self.audio = False
-        self.right_second = False
-        self.park_to_left = False
-        self.park_to_right = False
-        self.stop = False
-        self.collect = None
-        self.start_time = rospy.get_time() # 마커 동작을 수행할 때 딜레이를 주기 위해 마커를 인식한 시점에서의 시간을 저장
-        self.rate = rospy.Rate(5) # 1초에 5번 loop를 반복할 수 있도록 rate라는 객체를 생성
-        self.gtan = 0 # 두 차선의 기울기를 이용해 차선이 어느 한 쪽으로 치우친 정도를 저장
-        #pygame.mixer.init()
-        self.pub = rospy.Publisher("/limo/marker/cmd_vel", Twist, queue_size=5)
-        self.pub1 = rospy.Publisher("/limo/marker/bool", Bool, queue_size=5)
-        self.park_bool_pub = rospy.Publisher("/limo/marker/park", Bool, queue_size=5)
-        self.stop_bool_pub = rospy.Publisher("/limo/marker/stop", Bool, queue_size=5)
-        rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.marker_CB)
-        rospy.Subscriber("/limo/lane/gtan", Float64, self.global_gtan)
-        #self.play_mp3('/home/agilex/limo_project/src/limo_legend/test/start.mp3')
-
-    # lane_detect.py로부터 받아온 두 차선의 기울어진 정도에 따른 값을 받아옴
-    def global_gtan(self, _data):
-        self.gtan = _data.data
-        # print(self.gtan)
+        rospy.init_node("lane_detect")
+        self.YELLOW_LANE_LOW_TH = np.array([0, 90, 100]) # hls 색 영역의 최솟값 설정
+        self.YELLOW_LANE_HIGH_TH = np.array([60, 220, 255]) # hls 색 영역의 최댓값 설정
+        self.cvbridge = CvBridge() # 실시간 스트리밍
+        self.viz = rospy.get_param("~visualization", True)
+        rospy.Subscriber(rospy.get_param("~image_topic_name", "/camera/rgb/image_raw/compressed"), CompressedImage, self.image_topic_callback)
+        self.distance_pub1 = rospy.Publisher("/limo/lane_left", Int32, queue_size=5)
+        self.distance_pub2 = rospy.Publisher("/limo/lane_right", Int32, queue_size=5)
+        self.lane_connect_pub = rospy.Publisher("/limo/lane_connect", Bool, queue_size=5)
+        self.lane_accel_pub = rospy.Publisher("/limo/lane/accel", Bool, queue_size=5)
+        self.gtan_pub = rospy.Publisher("/limo/lane/gtan", Float64, queue_size=5)
     
-    # 인식한 마커와의 거리를 계산하고, 인식한 마커의 id값에 따른 문자열을 found_sign 함수에 전달
-    def marker_CB(self, data):
-        for marker in data.markers:
-            kim_x = marker.pose.pose.position.x
-            kim_y = marker.pose.pose.position.y
-            kim_z = marker.pose.pose.position.z
-            self.kim_distance = (kim_x**2+kim_y**2+kim_z**2)**0.5 # 마커와의 거리 계산
-            # print(self.kim_distance)
-            # print(marker.id)
-
-            if marker.id == 0:
-                self.found_sign("stop")
-            elif marker.id == 1:
-                if self.right_second == False:
-                    self.found_sign("right")
-                elif self.right_second == True:
-                    self.found_sign("right2")
-            elif marker.id == 3:
-                self.found_sign("park")
+    # 이미지 자르기
+    def imageCrop(self, _img=np.ndarray(shape=(480, 640))):
+        return _img[420:480, :] # 이미지 가로로 자르기
     
-    # 전달받은 문자열을 저장하고 특정 조건을 만족하면 동작 수행
-    def found_sign(self, _data):
-        self.collect = _data
-        #if self.flag == None: # 전달받은 마커 데이터가 없거나, 마커 동작 수행을 끝마쳐 self.flag에 아무 데이터가 없는 경우
-        if self.kim_distance > 0.83 and _data == "park": # 마커와의 거리가 0.8보다 큰데 park 신호가 왔을 경우
-            return
-        elif self.kim_distance > 0.86 and _data == "right": # 마커와의 거리가 0.8보다 큰데 stop 신호가 왔을 경우
-            # self.gtan < -0.5
-            return
-        else:
-            self.start_time = rospy.get_time()
-            self.flag = self.collect # 저장해둔 문자열을 self.flag로 전달
-
-    # 0번 마커(정지 신호)를 인식하였다면 아래의 동작 수행
-    def stop_sign(self):
-        if self.flag != "stop": # main함수에 의해 계속 실행되므로 stop 신호가 아니면 패스
-            return
-
-        passed_time = rospy.get_time() - self.start_time # 마커 동작을 수행한 시점으로부터 지난 시간
-        if passed_time > 1.2:
-            self.flag = None # 다음 마커 동작 수행을 위해 self.flag 초기화
-            #self.audio = False
-            # rospy.loginfo("STOP Marker End")
-        elif passed_time > 0.5:
-            self.override_twist = False # control.py에 마커 동작 수행이 끝났음을 알려줄 변수를 False로 전환
-        else:
-            # print("stop_start")
-            self.override_twist = True # control.py에 마커 동작 수행이 끝났음을 알려줄 변수를 True로 전환
-            self.stop = True
-            self.drive_data.linear.x = 0.0
-            self.drive_data.angular.z = 0.0
-            #if not self.audio:
-                #self.play_mp3('/home/agilex/limo_project/src/limo_legend/test/stop.mp3')
-                #self.audio = True
-
-    # 1번 마커(우회전 신호)를 인식하였다면 아래의 동작 수행
-    def right_turn_sign(self):
-        if self.flag != "right": # main함수에 의해 계속 실행되므로 right 신호가 아니면 패스
-            return
-        
-        passed_time = rospy.get_time() - self.start_time
-        if passed_time > 2.8:
-            self.flag = None
-            self.override_twist = False
-            #self.audio = False
-            # rospy.loginfo("RIGHT Marker End")
-        elif passed_time > 1.3:
-            # print("right_start")
-            self.override_twist = True
-            self.right_second = True
-            self.drive_data.linear.x = 0.0
-            self.drive_data.angular.z = -1.2
-            #if not self.audio:
-                #self.play_mp3('/home/agilex/limo_project/src/limo_legend/test/right.mp3')
-                #self.audio = True
-
-    # 주차구간 이후 횡단보도 쪽 우회전 마커를 인식할 경우 (gtan를 이용한 연산이 불가능)
-    def right2_turn_sign(self):
-        if self.flag != "right2":
-            return
-
-        passed_time = rospy.get_time() - self.start_time
-        if passed_time > 3.2:
-            self.flag = None
-            self.override_twist = False
-            #self.audio = False
-        elif passed_time > 1.6: # 오른쪽으로 제자리 회전
-            self.drive_data.linear.x = 0.0
-            self.drive_data.angular.z = -1.0
-            # print("rightright")
-            #if not self.audio:
-                #self.play_mp3('/home/agilex/limo_project/src/limo_legend/test/right.mp3')
-                #self.audio = True
-
-    # 3번 마커(주차 신호)를 인식하였다면 아래의 동작 수행
-    def park_sign(self):
-        if self.flag != "park": # main함수에 의해 계속 실행되므로 park 신호가 아니면 패스
-            return
-
-        passed_time = rospy.get_time() - self.start_time
-        #if passed_time > 7:
-            #self.flag = None # 다음 마커 동작 수행을 위해 self.flag 초기화
-            #self.override_twist = False # control.py에 마커 동작 수행이 끝났음을 알려줄 변수를 False로 전환
-            #self.park = False # 주차 마커 인식 여부를 False로 전환 (다시 가속 가능)
-            #self.audio = False
-            # rospy.loginfo("PARK Marker End")
-        if passed_time > 4: # 오른쪽으로 180도 제자리 회전
-            self.drive_data.linear.x = 0.0
-            self.drive_data.angular.z = 0.0
-        elif passed_time > 2.5: # gtan 값을 이용해 주차공간 내에서 정렬
-            if abs(self.gtan) < 0.0005:
-                self.drive_data.linear.x = 0.0
-                self.drive_data.angular.z = 0.0
-            else:
-                self.drive_data.linear.x = 0.0
-                self.drive_data.angular.z = 0.5
-        elif passed_time > 1.5: # 조금 직진하여 주차공간에 완벽히 진입
-            self.drive_data.linear.x = 0.2
-            self.drive_data.angular.z = 0.0
-        else: # 적절한 위치에서 우회전하여 주차공간에 진입
-            self.override_twist = True
-            #self.park_to_left = True
-            self.park = True # 주차 마커를 인식했음을 알림 (가속 차단 용도)
-            self.drive_data.linear.x = 0.3
-            self.drive_data.angular.z = -1.0
-            #if not self.audio:
-                #self.play_mp3('/home/agilex/limo_project/src/limo_legend/test/park.mp3')
-                #self.audio = True
-
-    #def play_mp3(self, file_path):
-        #pygame.mixer.music.load(file_path)
-        #pygame.mixer.music.play(0)
+    # 노란색 부분 추출
+    def colorDetect(self, _img=np.ndarray(shape=(480, 640))):
+        hls = cv2.cvtColor(_img, cv2.COLOR_BGR2HLS)
+        mask_yellow = cv2.inRange(hls, self.YELLOW_LANE_LOW_TH, self.YELLOW_LANE_HIGH_TH)
+        return mask_yellow
     
-    # 마커들의 동작을 우선순위를 두어 함수 실행 & 주행 데이터와 마커 인식 유무 데이터 퍼블리시
-    def main(self): # 마커 신호에 우선순위를 두었지만 사실 의미가 없다...
-        self.park_sign() # 주차 신호를 1순위로 실행
-        self.stop_sign() # 정지 신호를 2순위로 실행
-        self.right_turn_sign() # 우회전 신호를 3순위로 실행
-        self.right2_turn_sign()
-        self.pub.publish(self.drive_data) # 주행 데이터를 퍼블리시
-        self.pub1.publish(self.override_twist) # 마커 인식 여부를 담은 변수를 퍼블리시
-        self.park_bool_pub.publish(self.park) # 주차 마커 인식 여부를 담은 변수를 퍼블리시
-        self.stop_bool_pub.publish(self.stop)
-        self.rate.sleep() # 무한루프에서 설정한 주기를 맞추기 위해 기다리는 함수
+    # 차선의 무게중심 계산
+    def calcLaneDistance(self, _img=np.ndarray(shape=(480, 640))):
+        try:
+            M = cv2.moments(_img)
+            self.x = int(M['m10']/M['m00'])
+            self.y = int(M['m01']/M['m00'])
+        except:
+            self.x = -1
+            self.y = -1
+        return self.x
 
-if __name__ == "__main__":
-    try:
-        id_control = ID_control()
-        while not rospy.is_shutdown():
-            id_control.main()
-    except rospy.ROSInterruptException:
+    # 왼쪽 차선이 오른쪽 threshold 이미지에 표시될 경우 처리
+    def lane_connect(self, thresholded_image, thresholded_image2):
+        connected = np.sum(thresholded_image[:,319]) > 1 and np.sum(thresholded_image2[:,0]) > 1
+        # 왼쪽 아래 카메라의 가장 오른쪽 한 줄을 적분하여 1보다 크면(차선이 존재하면) True
+        # 오른쪽 아래 카메라의 가장 왼쪽 한 줄을 적분하여 1보다 크면(차선이 존재하면) True
+        # 위 두 개가 동시에 참인 경우(왼쪽 차선이 두 카메라 모두에 인식된 경우) True를 connected에 전달
+        self.lane_connect_pub.publish(connected)
+
+    # 두 차선이 인식될 경우 True를 반환 (가속 구간)
+    def lane_speed(self, thresholded_image, thresholded_image2):
+        accel = np.sum(thresholded_image[0,:]) > 1 and np.sum(thresholded_image2[0,:]) > 1
+        # 왼쪽 아래 카메라의 가장 윗부분 가로 한 줄을 적분하여 1보다 크면(차선이 존재하면) True
+        # 오른쪽 아래 카메라의 가장 윗부분 가로 한 줄을 적분하여 1보다 크면(차선이 존재하면) True
+        # 위 두 개가 동시에 참인 경우(양쪽 차선이 모두 존재하는 직진 구간인 경우) True를 accel에 전달
+        self.lane_accel_pub.publish(accel)
+
+    def global_tan(self, _img):
+        _img = np.where(_img, True, False) # 흑백 처리한 이미지는 0 또는 255를 이용하므로 0 또는 1로 변환
+        if np.sum(_img) == 0: # 이미지의 모든 픽셀 값 합산. 합이 0이면 이미지에 처리할 대상이 없는 것을 의미
+            return 0
+        x_range = np.arange(-320, 320) # x축 범위 설정. 이미지의 중심을 기준으로 왼쪽부터 오른쪽까지의 위치
+        y_range = np.arange(180, 240) # y축 범위 설정
+        matrix_x = _img * x_range # 이미지의 각 픽셀에 가로축 값들을 곱함. 픽셀 위치에 따른 가중치를 부여
+        matrix_y = (_img.T * y_range).T # 이미지의 세로줄에 가중치를 부여하기 위해 전치행렬 이용
+        x_not_zero = np.where(matrix_x, True, False)
+        matrix_x = np.where(x_not_zero, matrix_x, 1)
+        matrix_atan = np.where(x_not_zero, np.arctan(matrix_y / matrix_x), 0) # matrix_x가 참이면 arctan 연산결과를, 거짓이면 0을 반환
+        # 각 픽셀에서 y축 값과 x축 값의 비율 (픽셀의 기울기)를 계산하고 arctan를 이용해 탄젠트 값(기울기)을 각도(라디안 단위)로 변환
+        gtan = np.sum(matrix_atan) / np.sum(_img) # 차선의 평균 기울기 계산 (이미지 전체에서 감지된 차선의 기울기 / 이미지 내에서 차선으로 감지된 픽셀의 총 수)
+        return gtan
+    
+    # 화면에 출력
+    def visResult(self):
         pass
+        # cv2.circle(self.cropped_image, (self.x, self.y), 10, 255, -1)
+        # cv2.imshow("lane_original", self.frame)
+        # cv2.imshow("lane_thresholded_left", self.thresholded_image)
+        # cv2.imshow("lane_thresholded_right", self.thresholded_image2)
+        # cv2.waitKey(1)
+
+    def image_topic_callback(self, img):
+        self.frame = self.cvbridge.compressed_imgmsg_to_cv2(img, "bgr8")
+        self.cropped_image = self.imageCrop(self.frame)
+        self.thresholded_image_original = self.colorDetect(self.cropped_image)
+        self.thresholded_image = self.thresholded_image_original[:, 0:320] # 흑백처리된 가로로 자른 이미지를 세로로 자르기 (왼쪽 차선 카메라)
+        self.thresholded_image2 = self.thresholded_image_original[:, 320:640] # 흑백처리된 가로로 자른 이미지를 세로로 자르기 (오른쪽 차선 카메라)
+        self.left_distance = self.calcLaneDistance(self.thresholded_image)
+        self.right_distance = self.calcLaneDistance(self.thresholded_image2)
+        self.lane_connect(self.thresholded_image, self.thresholded_image2)
+        self.lane_speed(self.thresholded_image, self.thresholded_image2)
+        self.lane_gtan = self.global_tan(self.thresholded_image_original)
+        self.distance_pub1.publish(self.left_distance)
+        self.distance_pub2.publish(self.right_distance)
+        self.gtan_pub.publish(self.lane_gtan)
+        
+        if self.viz:
+            self.visResult()
+            
+def run():
+    new_class = LaneDetection()
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        run()
+    except KeyboardInterrupt:
+        print("program down")
