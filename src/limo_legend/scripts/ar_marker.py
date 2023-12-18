7a0ea4d28b519b1fa9d3f7659998cb597e480f8e
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-19 01:07:23 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -105,17 +105,16 @@ class ID_control:
             return
 
         passed_time = rospy.get_time() - self.start_time
-        if passed_time > 4:
+        if passed_time > 3.96:
             self.flag = None
             self.override_twist = False
             self.park_to_right = False
         elif passed_time > 2.7: # 오른쪽으로 제자리 회전
-            if self.kim_distance < 0.4:
-                self.override_twist = True
-                self.right_good = False
-                print("2222")
-                self.drive_data.linear.x = 0.15
-                self.drive_data.angular.z = -1.21
+            self.override_twist = True
+            self.right_good = False
+            print("2222")
+            self.drive_data.linear.x = 0.15
+            self.drive_data.angular.z = -1.21
 
     # 3번 마커(주차 신호)를 인식하였다면 아래의 동작 수행
     def park_sign(self):
