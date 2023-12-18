78b3cdab7294ca44b70a108dcd1c0899d60f4494
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-18 22:17:39 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -62,14 +62,14 @@ class ID_control:
     # 전달받은 문자열을 저장하고 특정 조건을 만족하면 동작 수행
     def found_sign(self, _data):
         self.collect = _data
-        if self.flag == None: # 전달받은 마커 데이터가 없거나, 마커 동작 수행을 끝마쳐 self.flag에 아무 데이터가 없는 경우
-            if self.kim_distance > 0.83 and _data == "park": # 마커와의 거리가 0.8보다 큰데 park 신호가 왔을 경우
-                return
-            elif self.gtan < -0.5 and _data == "right": # 마커와의 거리가 0.8보다 큰데 stop 신호가 왔을 경우
-                return
-            else:
-                self.start_time = rospy.get_time()
-                self.flag = self.collect # 저장해둔 문자열을 self.flag로 전달
+        #if self.flag == None: # 전달받은 마커 데이터가 없거나, 마커 동작 수행을 끝마쳐 self.flag에 아무 데이터가 없는 경우
+        if self.kim_distance > 0.83 and _data == "park": # 마커와의 거리가 0.8보다 큰데 park 신호가 왔을 경우
+            return
+        elif self.gtan < -0.5 and _data == "right": # 마커와의 거리가 0.8보다 큰데 stop 신호가 왔을 경우
+            return
+        else:
+            self.start_time = rospy.get_time()
+            self.flag = self.collect # 저장해둔 문자열을 self.flag로 전달
 
     # 0번 마커(정지 신호)를 인식하였다면 아래의 동작 수행
     def stop_sign(self):
