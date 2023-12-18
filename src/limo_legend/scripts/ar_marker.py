6b9b4c45b1a80697074db1af41e65562cfce473c
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-18 20:00:10 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -101,12 +101,12 @@ class ID_control:
             return
         
         passed_time = rospy.get_time() - self.start_time
-        if passed_time > 4.2:
+        if passed_time > 3.9:
             self.flag = None
             self.override_twist = False
             #self.audio = False
             # rospy.loginfo("RIGHT Marker End")
-        elif passed_time > 2.5:
+        elif passed_time > 2.2:
             # print("right_start")
             self.override_twist = True
             self.right_second = True
