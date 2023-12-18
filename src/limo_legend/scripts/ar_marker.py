5773b42d0028286b2a8b408684e788276c128f3a
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-18 22:52:18 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -104,11 +104,11 @@ class ID_control:
             return
 
         passed_time = rospy.get_time() - self.start_time
-        if passed_time > 4.5:
+        if passed_time > 4.7:
             self.flag = None
             self.override_twist = False
             self.park_to_right = False
-        elif passed_time > 3: # 오른쪽으로 제자리 회전
+        elif passed_time > 3.3: # 오른쪽으로 제자리 회전
             self.drive_data.linear.x = 0.0
             self.drive_data.angular.z = -1.0
         else: # 회전할 위치까지 전진
