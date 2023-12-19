b932d1dc8590c9c85c9f46f47405777fa5417bd0
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-19 15:56:33 +0900

    Update ar_marker.py

--- src/limo_legend/scripts/ar_marker.py
+++ src/limo_legend/scripts/ar_marker.py
@@ -86,9 +86,9 @@ class ID_control:
         if passed_time > 12:
             self.flag = None # 다음 마커 동작 수행을 위해 self.flag 초기화
             # rospy.loginfo("STOP Marker End")
-        elif passed_time > 5.5:
+        elif passed_time > 10:
             self.override_twist = False # control.py에 마커 동작 수행이 끝났음을 알려줄 변수를 False로 전환
-        else:
+        elif passed_time > 5:
             # print("stop_start")
             self.stop = True
             self.right_good = False
