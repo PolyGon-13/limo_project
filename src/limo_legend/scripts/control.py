dffa2925e9f1f221954570e9a22913b4c1f4405b
Donghyun Kim <107293272+PolyGon-13@users.noreply.github.com>
2023-12-19 15:56:41 +0900

    Update control.py

--- src/limo_legend/scripts/control.py
+++ src/limo_legend/scripts/control.py
@@ -142,12 +142,6 @@ class LimoController:
             if self.override_twist == True: # 마커를 인식한 경우
                 # if self.e_stop != "Warning": # 라이다가 장애물을 감지하지 않았을 경우 (표지판을 장애물로 인식하는 경우가 있어서 주석처리함)
                 drive_data = self.new_drive_data # 마커 동작을 수행
-            elif self.lane_connected == False and self.accel_bool == True: # 왼쪽 차선이 2번째 카메라(오른쪽 차선)에 침범하지 않았으며 두 차선을 인식한 경우
-                if abs(drive_data.angular.z) < 0.3 and self.park_bool == False:
-                    if self.stop_bool == True:
-                    # 계산된 각속도가 0.3보다 작을 때(가속을 하면 안되는 구간에서도 두 차선을 인식하는 경우가 발생하기 때문에 사용)
-                    # 또한 주차 마커를 인식하지 않은 경우(교차로 구간에서 가속을 하는 구간이 발생하는데 주차 모션 제어가 방해가 됨)
-                        drive_data.linear.x *= 1.3 # 기존 속도의 1.3배로 달림
 
             # IMU 센서 동작
             if abs(self.angular_y) > 0.05:
