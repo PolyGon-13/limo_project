1d0ebb690c194ed209663fe7beb627677b7eec9d
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-08-29 22:08:07 +0900

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agilex/limo_project/src
-# Build directory: /home/agilex/limo_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("limo_legend")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/agilex/limo_project/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/agilex/limo_project/devel;/home/agilex/limo_ws/devel;/opt/ros/melodic'.split(';') if '/home/agilex/limo_project/devel;/home/agilex/limo_ws/devel;/opt/ros/melodic' != '' else []
